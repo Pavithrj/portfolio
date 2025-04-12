4f2825b1d273a9704162b129c3d4c58b98f9c724
Pavithr Jain <pavithrjain19@gmail.com>
2025-04-12 18:31:41 +0530

    added icons

---
+++ src/components/icons/index.js
@@ -0,0 +1,40 @@
+import htmlIcon from './Skills/HTMLIcon';
+import CSSIcon from './Skills/CSSIcon';
+import JavaScriptIcon from './Skills/JavaScriptIcon';
+import ReactJsIcon from './Skills/ReactJsIcon';
+import NodeJsIcon from './Skills/NodeJsIcon';
+import ExpressJsIcon from './Skills/ExpressJsIcon';
+import MongoDBIcon from './Skills/MongoDBIcon';
+
+const iconsData = [
+    {
+        name: "HTML",
+        svg: htmlIcon,
+    },
+    {
+        name: "CSS",
+        svg: ReactJsIcon,
+    },
+    {
+        name: "JavaScript",
+        svg: ReactJsIcon,
+    },
+    {
+        name: "ReactJs",
+        svg: ReactJsIcon,
+    },
+    {
+        name: "NodeJs",
+        svg: ReactJsIcon,
+    },
+    {
+        name: "ExpressJs",
+        svg: ReactJsIcon,
+    },
+    {
+        name: "MongoDB",
+        svg: ReactJsIcon,
+    },
+];
+
+export default iconsData;
--- src/data/projects/ProjectsData.js
+++ src/data/projects/ProjectsData.js
@@ -1,3 +1,4 @@
+
 const projects = [
     {
         title: "Portfolio Website",
