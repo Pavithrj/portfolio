0350b4c9a930fdc4eeca37fcde390cc26691f388
Pavithr Jain <pavithrjain19@gmail.com>
2025-04-18 10:43:07 +0530

    projects section

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import ProjectsData from './../../data/projects/ProjectsData';
 
 const Projects = () => {
