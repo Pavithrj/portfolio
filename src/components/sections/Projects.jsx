4b5f5f9c5ec54384038c8e93366b6b254e5c230a
Pavithr Jain <pavithrjain19@gmail.com>
2025-04-18 10:42:46 +0530

    projects section

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -33,7 +33,7 @@ const Projects = () => {
             <div className="absolute w-56 h-56 bg-blue-500 rounded-full top-1/4 left-1/4 sm:w-72 sm:h-72 opacity-20 blur-3xl"></div>
             <div className="absolute w-56 h-56 bg-purple-500 rounded-full bottom-1/4 right-1/4 sm:w-72 sm:h-72 opacity-20 blur-3xl"></div>
 
-            <div className='flex flex-col items-center w-full gap-6 px-4 py-8 animate-fade-in'>
+            <div className="flex flex-col items-center w-full gap-6 px-4 py-8 animate-fade-in">
                 <h2 className="text-3xl font-extrabold leading-tight tracking-wide text-blue-400 duration-500 sm:text-4xl md:text-5xl lg:text-6xl animate-fade-in">
                     Projects
                 </h2>
