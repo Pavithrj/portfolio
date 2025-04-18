e59ba698c20e0504285a999821ecc42a149f73cc
Pavithr Jain <pavithrjain19@gmail.com>
2025-04-18 09:58:22 +0530

    app portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import AllProjects from './pages/AllProjects';
-import Navbar from './components/layout/Navbar';
-import Home from './components/sections/Home';
-import About from './components/sections/About';
-import Skills from './components/sections/Skills';
-import Projects from './components/sections/Projects';
-import Contact from './components/sections/Contact';
-import Footer from './components/layout/Footer';
+import Portfolio from './pages/Portfolio';
 
 function App() {
     return (
         <Routes>
-            <Route path="/portfolio" element={
-                <>
-                    <Navbar />
-                    <Home />
-                    <About />
-                    <Skills />
-                    <Projects />
-                    {/* <Contact /> */}
-                    {/* <Footer /> */}
-                </>
-            } />
-
+            <Route path="/portfolio" element={<Portfolio />} />
             <Route path="/portfolio/projects" element={<AllProjects />} />
         </Routes>
     )
---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,27 @@
+import Navbar from './../components/layout/Navbar';
+import Home from './../components/sections/Home';
+import About from './../components/sections/About';
+import Skills from './../components/sections/Skills';
+import Projects from './../components/sections/Projects';
+import Contact from './../components/sections/Contact';
+import Footer from './../components/layout/Footer';
+
+const Portfolio = () => {
+    const sections = [Home, About, Skills, Projects];
+    // const sections = [Projects];
+    // const sections = [Contact];
+
+    return (
+        <div>
+            <Navbar />
+
+            {sections.map((Section, index) => (
+                <Section key={index} />
+            ))}
+
+            {/* <Footer /> */}
+        </div>
+    )
+};
+
+export default Portfolio;
