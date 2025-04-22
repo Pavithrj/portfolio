5ecf38aea3c72dfb668017c4624cbf470208327a
Pavithr Jain <pavithrjain19@gmail.com>
2025-04-22 09:21:13 +0530

    changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
                 fadeIn: {
                     "0%": { opacity: 0, transform: "translateY(-10px)" },
                     "100%": { opacity: 1, transform: "translateY(0)" },
-                },
+                }
             },
             animation: {
                 fadeIn: "fadeIn 1s ease-in-out forwards",
