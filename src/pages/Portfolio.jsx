4026c3157bf788e8aaba0dbc89b3015a82538c8f
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-04-26 10:54:31 +0000

    changed

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,10 +2,10 @@ import Navbar from './../components/layout/Navbar';
 import Footer from './../components/layout/Footer';
 
 import Home from '../components/sections/Home';
-import About from './../components/sections/About';
-import Skills from './../components/sections/Skills';
-import Projects from './../components/sections/Projects';
-import Contact from './../components/sections/Contact';
+import About from '../components/sections/About';
+import Skills from '../components/sections/Skills';
+import Projects from '../components/sections/Projects';
+import Contact from '../components/sections/Contact';
 
 const Portfolio = () => {
     const sections = [Home, About, Skills, Projects];
