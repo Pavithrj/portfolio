274a1a5a241f548e30899603e472049d6d71379a
Pavithr Jain <pavithrjain19@gmail.com>
2025-05-14 09:01:46 +0530

    change

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import HamburgerIcon from '../icons/navbar/HamburgerIcon';
 import CloseIcon from '../icons/navbar/CloseIcon';
-import PavithrJainLogo from './../../assets/navbar/PavithrJainLogo.png';
+import PavithrJainLogo from './../../assets/navbar/pj-logo.png';
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
