e672fd9e6be0672725141934bdd71f302daf970c
Pavithr Jain <pavithrjain19@gmail.com>
2025-06-03 20:26:29 +0530

    change

--- src/assets/projects/index.js
+++ src/assets/projects/index.js
@@ -1,9 +1,11 @@
 import live_calendar from './live-calendar.png';
+import chatwise from './chatwise.png';
 import imageOne from './imageOne.png';
 import imageTwo from './imageTwo.png';
 
 const images = {
     live_calendar,
+    chatwise,
     imageOne,
     imageTwo,
 };
