e09b5319494ac63faa58e36274b6461e27d56a5f
Pavithr Jain <pavithrjain19@gmail.com>
2025-11-21 19:44:34 +0530

    196

--- src/assets/projects/index.js
+++ src/assets/projects/index.js
@@ -1,11 +1,13 @@
 import live_calendar from './live-calendar.png';
 import chatwise from './chatwise.png';
+import devrix from './devrix.png';
 import imageOne from './imageOne.png';
 import imageTwo from './imageTwo.png';
 
 const images = {
     live_calendar,
     chatwise,
+    devrix,
     imageOne,
     imageTwo,
 };
